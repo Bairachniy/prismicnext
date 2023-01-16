b59cedb59f5d1488bd1b687b396afa62f951d129
Bairachniy <bairachniy@gmail.com>
2023-01-16 13:00:06 +0200

    second commit

---
+++ nextjs-blog/.slicemachine/assets/slices/TrySlice/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/TrySlice';
+
+export default {
+  title: 'slices/TrySlice'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{"datatoday":"2015-11-26"}],"primary":{"title":[{"type":"heading1","text":"Statement","spans":[]}],"description":[{"type":"paragraph","text":"Lorem anim dolor irure incididunt ut adipisicing do eiusmod aliquip sint cupidatat mollit elit magna proident.","spans":[]}],"imaga":{"dimensions":{"width":4000,"height":4000},"alt":null,"copyright":null,"url":"https://images.unsplash.com/photo-1591012911207-0dbac31f37da"}},"id":"_Default","slice_type":"try_slice"}} />
+_Default.storyName = ''
